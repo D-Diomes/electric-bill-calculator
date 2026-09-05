0819125b0dbe226224bd7eece0c8fc964037fa7f
D-Diomes <diomesd09@gmail.com>
2026-09-05 06:21:12 +0000

    fixed the bugs

--- problematic_file.py
+++ problematic_file.py
@@ -4,22 +4,24 @@ name = input("Enter resident name: ")
 consumption = input("Enter kWh consumed this month: ")
 is_senior = input("Senior citizen? (yes/no): ")
 
+consumption = int(consumption)
+
 if consumption <= 100:
     rate = 9.00
-elif consumption < 200
+elif consumption <= 200:
     rate = 11.00
 else:
     rate = 14.00
 
 total = consumption * rate
 
-if is_senior = "yes"
+if is_senior == "yes":
     discount = total * 0.05
     total = total - discount
-    print("Senior discount applied: ₱" + discount)
+    print(f"Senior discount applied: ₱{discount}")
 
 print("----- ELECTRIC BILL -----")
-print("Name: " + name)
-print("Consumption: " + consumption + " kWh")
-print("Rate applied: ₱" rate "/kWh")
-print("Total Due: ₱" + total)
+print(f"Name: {name}")
+print(f"Consumption: {consumption}kWh")
+print(f"Rate applied: ₱{rate} /kWh")
+print(f"Total Due: ₱{total}")
